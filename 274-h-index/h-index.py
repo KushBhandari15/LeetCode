dfa5543db9404fc4204c6229da71771761688232
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2025-04-29 10:40:13 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (66.05%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -4,18 +4,19 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-
-        maximum = max(citations)
-        res = 0
-        for i in range (maximum+1):
-            count = 0
-            for j in citations:
-                if (j >= i):
-                    count += 1
-            
-            if (count >= i):
-                res = max(i, res)
         
-        return res
+        size = len(citations)
+        paper_count = [0] * (size+1)
 
+        for i in citations:
+            paper_count[min(size, i)] += 1
         
+
+        h = size
+        papers = paper_count[size]
+
+        while (papers < h):
+            h -= 1
+            papers += paper_count[h]
+        
+        return h
