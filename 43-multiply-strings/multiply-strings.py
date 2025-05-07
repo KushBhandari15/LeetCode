13cb6cde9b4530404173f1a1fddcc4a682dc5c27
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2025-05-07 02:39:40 -0400

    Time: 3 ms (56.37%) | Memory: 12.5 MB (13.15%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -8,7 +8,10 @@ class Solution(object):
         
         helperMap = {"0": 0, "1" : 1, "2": 2, "3": 3,
                     "4" : 4, "5": 5, "6": 6,
-                    "7": 7, "8": 8, "9": 9}
+                    "7": 7, "8": 8, "9": 9,
+                    0: "0", 1: "1", 2: "2", 3:"3",
+                    4: "4", 5: "5", 6: "6", 7: "7",
+                    8: "8", 9: "9"}
 
         number1 = 0
         for i in range (len(num1)):
@@ -26,8 +29,9 @@ class Solution(object):
         # Convert result back to string manually
         result_str = ""
         while res > 0:
-            result_str = chr(res % 10 + ord('0')) + result_str
-            res = res // 10
+            temp = res%10
+            result_str = helperMap[temp] + result_str
+            res = res//10
         
         return result_str
 
