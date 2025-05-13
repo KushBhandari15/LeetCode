baac47c893299284242cc33e529db8c31d5e7fdf
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2025-05-13 06:33:48 -0400

    Time: 1 ms (46.41%) | Memory: 12.4 MB (86.41%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -15,31 +15,36 @@ class Solution(object):
             for j in range(col):
                 count = 0
                 #Up
-                if (i-1 >= 0 and board[i-1][j] == 1): count += 1
+                if (i-1 >= 0 and (board[i-1][j] == 1 or board[i-1][j] == -1)): count += 1
                 #Down
-                if (i+1 < row and board[i+1][j] == 1): count += 1
+                if (i+1 < row and (board[i+1][j] == 1 or board[i+1][j] == -1)): count += 1
                 #Left
-                if (j-1 >= 0 and board[i][j-1] == 1): count += 1
+                if (j-1 >= 0 and (board[i][j-1] == 1 or board[i][j-1] == -1)): count += 1
                 #Right
-                if (j+1 < col and board[i][j+1] == 1): count += 1
+                if (j+1 < col and (board[i][j+1] == 1 or board[i][j+1] == -1)): count += 1
                 #DiagonalL1
-                if (i-1 >= 0 and j-1 >= 0 and board[i-1][j-1] == 1): count += 1
+                if (i-1 >= 0 and j-1 >= 0 and (board[i-1][j-1] == 1 or board[i-1][j-1] == -1)): count += 1
                 #DiagonalL2
-                if (i+1 < row and j-1 >= 0 and board[i+1][j-1] == 1): count += 1
+                if (i+1 < row and j-1 >= 0 and (board[i+1][j-1] == 1 or board[i+1][j-1] == -1)): count += 1
                 #DiagonalL3
-                if (i-1 >= 0 and j+1 < col and board[i-1][j+1] == 1): count += 1
+                if (i-1 >= 0 and j+1 < col and (board[i-1][j+1] == 1 or board[i-1][j+1] == -1)): count += 1
                 #DiagonalL4
-                if (i+1 < row and j+1 < col and board[i+1][j+1] == 1): count += 1
+                if (i+1 < row and j+1 < col and (board[i+1][j+1] == 1 or board[i+1][j+1] == -1)): count += 1
 
-                if (count == 2 and board[i][j] == 1):
-                    helper.add((i,j))
-                
-                if (count == 3):
-                    helper.add((i,j))
+                if (count == 2):
+                    pass
+                elif (count == 3):
+                    if (board[i][j] == 0):
+                        board[i][j] = 2
+
+                else:
+                    if (board[i][j] == 1):
+                        board[i][j] = -1     
+
 
         for i in range(row):
             for j in range(col):
-                if (i,j) in helper:
-                    board[i][j] = 1
-                else:
+                if board[i][j] == -1:
                     board[i][j] = 0
+                elif board[i][j] == 2:
+                    board[i][j] = 1
