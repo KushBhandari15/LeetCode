aed5ac4a8686b2fc6616a18b781834a66aa3202c
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2025-05-13 08:04:34 -0400

    Time: 23 ms (54.97%) | Memory: 14.4 MB (14.63%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if (len(s) != len(t)):
+            return False
+        
+        map1 = {}
+        map2 = {}
+        for i in range(len(s)):
+            
+            map1[s[i]] = map1.get(s[i], 0) + 1
+            map2[t[i]] = map2.get(t[i], 0) + 1
+
+
+        return map1==map2
