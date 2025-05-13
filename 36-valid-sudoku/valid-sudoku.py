4d1fcefbe3ad53e015ad9178931778c61c7fdad2
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2025-05-13 03:00:09 -0400

    Time: 231 ms (5.09%) | Memory: 12.4 MB (54.69%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         
         
-        rows = [[] for _ in range(9)]
-        cols = [[] for _ in range(9)]
-        box = [[] for _ in range(9)]
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        box = [set() for _ in range(9)]
 
         for i in range (9):
             for j in range (9):
@@ -20,9 +20,9 @@ class Solution(object):
                 if (board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in box[boxNum]):
                     return False
                 
-                rows[i].append(board[i][j])
-                cols[j].append(board[i][j])
-                box[boxNum].append(board[i][j])
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                box[boxNum].add(board[i][j])
                 # print(rows)
                 # print(cols)
                 print(box)
