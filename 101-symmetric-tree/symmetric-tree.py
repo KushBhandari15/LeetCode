2f561e04060e4c393011db401780a00ffe6cf067
Kush Bhandari <98527317+KushBhandari15@users.noreply.github.com>
2025-06-13 03:36:50 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (34.47%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -7,8 +7,6 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         
-        if not root:
-            return True
 
         def helper(n1, n2):
 
@@ -16,8 +14,11 @@ class Solution:
                 return True
             if not n1 or not n2:
                 return False
-
-            return (n1.val == n2.val and helper(n1.left, n2.right) 
-                and helper(n1.right, n2.left))
+            
+            if n1.val != n2.val:
+                return False
+            
+            return helper(n1.left, n2.right) and helper(n1.right, n2.left)
+        
         
         return helper(root.left, root.right)
